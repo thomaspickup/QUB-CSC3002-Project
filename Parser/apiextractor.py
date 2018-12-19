6f94aca3b4e5d68ac9db57abbfb914519caf23a0
Thomas Pickup <hello@thomaspickup.co.uk>
2018-12-19 22:43:36 +0000

    Now loops through the apis and counts the amount of time it is called

--- Parser/apiextractor.py
+++ Parser/apiextractor.py
@@ -46,12 +46,34 @@ def main():
     # Appends headers to dataset
     dataset.append(headers)
 
-    # Loops through all the files again
-        # Adds malware type and ID to pythong list
+    for file in json_filepaths:
+        # Creates empty array to store results
+        row = []
+
+        # Adds malware type and ID to python list
+        filename = file.replace('.json', '').replace(directory, '').replace('/','')
+        familyID = filename.split('-')[0]
+        sampleID = filename.split('-')[1]
+        row.append(familyID)
+        row.append(sampleID)
+
+        # Gets json file
+        data = getJSON(filepath)
+
         # Loops through all the api_names
+        for api in api_names:
+            count = 0
             # Counts how many times this api was called
+            for processes in data['behavior']['processes']:
+                for call in processes['calls']:
+                    if call['api'] == api:
+                        count += 1
+
             # Adds result to python list ['api_name', 'times_called']
+            row.append(count)
+            
         # Adds list row to table
+        dataset.append(row)
 
     # Exports table to csv file
     dataset_csv = directory + "/dataset.csv"
