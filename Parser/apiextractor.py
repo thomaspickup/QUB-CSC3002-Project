436ee1cb8c7db62d5cf55556eab6a255bd6602cb
Thomas Pickup <hello@thomaspickup.co.uk>
2018-12-19 21:43:41 +0000

    APIExtractor.py now traverses through directory and finds files ending with .json

--- Parser/apiextractor.py
+++ Parser/apiextractor.py
@@ -1,10 +1,16 @@
-import json
+import json, os
 
 def main():
-    with open("/Users/thomaspickup/Documents/University/CSC3002/Assignment/CSC3002-Project/Malware Reports/report.json") as f:
-        data = json.load(f)
+    directory = '/Users/thomaspickup/Documents/University/CSC3002/Assignment/CSC3002-Project/Malware Reports'
 
-    print(data['info']['id'])
+    for file in os.listdir(directory):
+        if file.endswith('.json'):
+            filepath = os.path.join(directory, file)
+
+            with open(filepath) as f:
+                data = json.load(f)
+
+            print(data['info']['id'])
 
 if __name__ == "__main__":
     main()
