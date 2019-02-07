50d4577c142849feff062e61cf8b33a8e5f68093
Thomas Pickup <thomaspickup@Thomass-MacBook-Pro.local>
2019-02-07 09:36:16 +0000

    datasetlist.py accepts sample and output directory input

--- Parser/datasetlist.py
+++ Parser/datasetlist.py
@@ -1,2 +1,14 @@
+import sys, os, csv
+
 def main():
-    
+    sample_directory = ""
+    output_directory = ""
+
+    if len(sys.argv) == 3:
+        sample_directory = sys.argv[1]
+        output_directory = sys.argv[2]
+
+    file_names = ['malware_types.csv', 'sample_list.csv']
+
+if __name__ == "__main__":
+    main()
