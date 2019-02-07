ab55471526d195dc6ab76bd6a60bf4ea1ebd7c81
Thomas Pickup <hello@thomaspickup.co.uk>
2019-02-07 09:54:45 +0000

    datasetlist.py now prints file names and base path

--- Parser/datasetlist.py
+++ Parser/datasetlist.py
@@ -7,8 +7,17 @@ def main():
     if len(sys.argv) == 3:
         sample_directory = sys.argv[1]
         output_directory = sys.argv[2]
+    else:
+        sample_directory = "/samples"
+        output_directory = "/output"
 
     file_names = ['malware_types.csv', 'sample_list.csv']
+    print(sample_directory)
+    if os.path.isdir(sample_directory):
+        for root, dirs, files in os.walk(sample_directory):
+            for file in files:
+                print(file)
+                print(os.path.basename(root))
 
 if __name__ == "__main__":
     main()
