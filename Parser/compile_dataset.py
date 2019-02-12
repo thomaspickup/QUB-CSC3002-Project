4209b8f961d39818a1409ee4b3791823e26dd49a
Thomas Pickup <hello@thomaspickup.co.uk>
2019-02-12 10:44:46 +0000

    Added compile_dataset.py to run datasetlist.py in my environment

---
+++ Parser/compile_dataset.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("python datasetlist.py '/Users/thomaspickup/documents/university/csc3002/assignment/samples' '/Users/thomaspickup/documents/university/csc3002/assignment/csc3002-project/dataset'")
--- Parser/datasetlist.py
+++ Parser/datasetlist.py
@@ -12,12 +12,53 @@ def main():
         output_directory = "/output"
 
     file_names = ['malware_types.csv', 'sample_list.csv']
-    print(sample_directory)
+
+    sample_types = []
+    excluded_files = [".DS_Store"]
+
     if os.path.isdir(sample_directory):
         for root, dirs, files in os.walk(sample_directory):
             for file in files:
-                print(file)
-                print(os.path.basename(root))
+                if not file in excluded_files:
+                    sample_types.append([file, os.path.basename(root)])
 
+    malware_types = []
+    sample_list = []
+    next_malware_id = 0
+    next_sample_id = 0
+
+    for st in sample_types:
+        malware_id = ""
+        id_found = False
+
+        for type in malware_types:
+            if type[1] == st[1]:
+                malware_id = str(type[0])
+                id_found = True
+
+        if id_found == False:
+            next_malware_id = next_malware_id + 1
+            malware_id = str(next_malware_id)
+            malware_types.append([malware_id, st[1]])
+
+        next_sample_id = next_sample_id + 1
+        sample_list.append([next_sample_id, malware_id, st[0]])
+
+    if os.path.isdir(output_directory):
+        type_name = file_names[0]
+        type_csv = os.path.join(output_directory, type_name)
+
+        sample_name = file_names[1]
+        sample_csv = os.path.join(output_directory, sample_name)
+
+        with open(type_csv, "w") as csv_file:
+            writer = csv.writer(csv_file, lineterminator='\n')
+            writer.writerows(malware_types)
+
+        with open(sample_csv, "w") as csv_file:
+            writer = csv.writer(csv_file, lineterminator='\n')
+            writer.writerows(sample_list)
+
+    print("~~ DataSet List Production Complete ~~")
 if __name__ == "__main__":
     main()
