09f8f4b63031c86907cae55bd6a067b1988b1e1a
Thomas Pickup <hello@thomaspickup.co.uk>
2019-02-14 12:08:18 +0000

    Rewrite

--- Parser/compile_dataset.py
+++ Parser/compile_dataset.py
@@ -1,7 +1,11 @@
 import os
 
 print("~~ API Results Retrieval: Started ~~")
+<<<<<<< HEAD
 os.system("python retrieve_apiresults.py '/Users/thomaspickup/Documents/University/CSC3002/Assignment/Cuckoo-export' '/Users/thomaspickup/Documents/University/CSC3002/Assignment/Malware-Reports'")
+=======
+os.system("python retrieve_apiresults.py '/Users/thomaspickup/Documents/University/CSC3002/Assignment/Cuckoo-export' '/Users/thomaspickup/Documents/University/CSC3002/Assignment/CSC3002-Project/Malware-Reports'")
+>>>>>>> Added test set for testing entire process, made tweeks to scripts
 
 print("~~ Sample List Production: Started ~~")
 os.system("python compile_samplelist.py '/Users/thomaspickup/documents/university/csc3002/assignment/samples' '/Users/thomaspickup/documents/university/csc3002/assignment/csc3002-project/dataset'")
